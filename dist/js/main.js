b5b571d5d18ffa49050925d07f69e6b1704cbda2
Roope Myller <roope.myller@gmail.com>
2023-05-07 11:31:41 +0300

    some minor fixes, converted filenames to english, overall ready commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,7 @@
 
 const menuButton = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
 const menuNav = document.querySelector('.menu-nav');
 
 const navItems = document.querySelectorAll('.nav-item');
@@ -15,6 +16,7 @@ function toggleMenu(){
     if(!showMenu){
         menuButton.classList.add('close');
         menu.classList.add('show');
+        menuBranding.classList.add('show');
         menuNav.classList.add('show');
 
         navItems.forEach(item => item.classList.add('show'))
@@ -26,6 +28,7 @@ function toggleMenu(){
     }else{
         menuButton.classList.remove('close');
         menu.classList.remove('show');
+        menuBranding.classList.remove('show');
         menuNav.classList.remove('show');
 
         navItems.forEach(item => item.classList.remove('show'))
@@ -34,6 +37,8 @@ function toggleMenu(){
         showMenu = false;
     }
 }
+
+
 
 
 
